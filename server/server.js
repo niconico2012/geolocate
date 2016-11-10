af4a76f58272147909bfb11f6e87dfa142373bed
JJ Estacio <johnathan.estacio@gmail.com>
2016-11-10 15:32:22 -0800

    Changed to mysql

--- server/server.js
+++ server/server.js
@@ -1,18 +1,33 @@
 import express from 'express';
-import {MongoClient} from 'mongodb'
 import path from 'path';
 import http from 'http';
+import mysql from 'mysql';
 
 const app = express();
 const PORT = 3000;
+
+// Setup mysql connection
+const connection = mysql.createConnection({
+	host: 'localhost',
+	user : 'root',
+	password: 'm117',
+	database: 'M117'
+});
 
 app.get('/', ( req, res ) => {
     res.sendFile(path.join(__dirname, 'hello.html'));
 });
 
-//MongoClient.connect('mongodb://localhost/users',(err, dbConnection) => {
-//    db = dbConnection;
-    app.listen(PORT, () => {
-        console.log("Started server at port", PORT);
-    });
-//})
+app.listen(PORT, () => {
+    console.log("Started server at port", PORT);
+
+    // Connect to mysql database
+    connection.connect();
+
+    // // Test mysql connection
+    // connection.query('SELECT * FROM test', ( err, rows, fields ) => {
+    // 	if (err) throw err;
+
+    // 	console.log(rows[0]);
+    // });
+});
