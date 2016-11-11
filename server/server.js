c294bb65ca6f4fb9beee3de63b34270634b3ccda
Helen Lee <helenlee@wifi-131-179-42-171.host.ucla.edu>
2016-11-10 16:40:12 -0800

    Added /report, /find functionalities

--- server/server.js
+++ server/server.js
@@ -5,11 +5,14 @@
 const express = require('express');
 const crypto = require('crypto');
 const sqlite = require('sqlite3').verbose();
+const bodyparser = require('body-parser');
 let db = new sqlite.Database('./data.db');
 
 
 const app = express();
 const PORT = 3000;
+
+app.use(bodyparser.urlencoded());
 
 app.get('/', ( req, res ) => {
     res.sendFile(path.join(__dirname, 'hello.html'));
@@ -22,6 +25,36 @@ app.post('/provision', ( req, res ) => {
         stmt.run(buf.toString('hex'));
         res.json({name: buf.toString('hex')});
     })
+});
+
+app.post('/report', ( req, res )=> {
+    var name = req.body.name || null;
+    var location = req.body.location || null;
+
+    if (!name || !location) {
+        res.json({success: false});
+    }
+    else {
+        db.run("UPDATE devices SET location=?, time=? WHERE name=?", location, Date.now(), name , (err) => {
+            if (err)
+                console.log("/report err: ", err);
+
+            res.json({success: err === null});
+        });
+    }
+});
+
+app.get('/find/:name', ( req, res ) => {
+    var findName= req.params.name || null;
+    db.all("SELECT * FROM devices WHERE name=?", findName, (err, rows) => {
+        if (err !== null || rows.length !== 1) {
+            console.log(err, rows);
+            res.json({success: false});
+        }
+        else {
+            res.json({success: true, device: rows[0]});
+        }
+    });
 });
 
 app.listen(PORT, () => {
