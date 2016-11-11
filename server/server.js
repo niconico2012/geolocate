d88c018fa01c833848300ab88d49396621b9ff76
Helen Lee <helenlee@wifi-131-179-42-171.host.ucla.edu>
2016-11-10 16:06:08 -0800

    added db, sqlite, and /provision

--- server/server.js
+++ server/server.js
@@ -1,28 +1,34 @@
-import express from 'express';
-import path from 'path';
-import http from 'http';
-import mysql from 'mysql';
+// import express from 'express';
+// import path from 'path';
+// import http from 'http';
+
+const express = require('express');
+const crypto = require('crypto');
+const sqlite = require('sqlite3').verbose();
+let db = new sqlite.Database('./data.db');
+
 
 const app = express();
 const PORT = 3000;
 
-// Setup mysql connection
-const connection = mysql.createConnection({
-	host: 'localhost',
-	user : 'root',
-	password: 'm117',
-	database: 'M117'
-});
-
 app.get('/', ( req, res ) => {
     res.sendFile(path.join(__dirname, 'hello.html'));
+});
+
+app.post('/provision', ( req, res ) => {
+    crypto.randomBytes(5, (err, buf) => {
+        if (err) throw err;
+        let stmt = db.prepare("INSERT INTO devices(name) VALUES(?)");
+        stmt.run(buf.toString('hex'));
+        res.json({name: buf.toString('hex')});
+    })
 });
 
 app.listen(PORT, () => {
     console.log("Started server at port", PORT);
 
     // Connect to mysql database
-    connection.connect();
+    // connection.connect();
 
     // // Test mysql connection
     // connection.query('SELECT * FROM test', ( err, rows, fields ) => {
@@ -31,3 +37,4 @@ app.listen(PORT, () => {
     // 	console.log(rows[0]);
     // });
 });
+
