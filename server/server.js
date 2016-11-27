5e1b0622b084d88dd276211955e824025aed7886
JJ Estacio <johnathan.estacio@gmail.com>
2016-11-26 16:43:51 -0800

    Fixed server.js

--- server/server.js
+++ server/server.js
@@ -7,14 +7,17 @@ var db = new sqlite.Database('./data.db');
 var app = express();
 var PORT = process.env.PORT || 4000;
 
-app.use(bodyparser.urlencoded());
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
 app.get('/', ( req, res ) => {
     res.json('sup');
 });
 app.post('/provision', ( req, res ) => {
     console.log('/provision received')
+
     crypto.randomBytes(5, (err, buf) => {
         if (err) throw err;
+        // db.run("INSERT INTO devices(name) VALUES(?)", "5C:F9:38:B2:21:20");
         var stmt = db.prepare("INSERT INTO devices(name) VALUES(?)");
         stmt.run(buf.toString('hex'));
         res.json({name: buf.toString('hex')});
@@ -25,6 +28,8 @@ app.post('/report', ( req, res )=> {
     console.log('/report received')
     var name = req.body.name || null;
     var location = req.body.location || null;
+
+    location = location.latitude + ' ' + location.longitude;
 
     if (!name || !location) {
         res.json({success: false});
@@ -43,7 +48,8 @@ app.get('/find/:name', ( req, res ) => {
     console.log('/find received')
     var findName= req.params.name || null;
     db.all("SELECT * FROM devices WHERE name=?", findName, (err, rows) => {
-        if (err !== null || rows.length !== 1) {
+        // if (err !== null || rows.length !== 1) {
+        if (err !== null) {
             console.log(err, rows);
             res.json({success: false});
         }
